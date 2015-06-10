f9493e0be6efb0e04950b5272b13c449df7aad56
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2015-06-10 23:36:15 +0300

    simple test

--- test/test.js
+++ test/test.js
@@ -1,22 +1,12 @@
-var assert = require("assert");
-
 var jspm = require("jspm");
-function jspmImport(path){
-    var module = '';
-    before(function() { return jspm.import(path).then(function(m){ module = m; }); });
-    return module;
-}
+var expect = require('chai').expect;
 
-var React = jspmImport('react').default;
+describe('Component', () => {
+  before(() => jspm.import('react').then(m => this.React = m));
+  before(() => jspm.import('lib/main').then(m => this.App = m.default));
 
-describe('Array', function(){
-    describe('#indexOf()', function(){
-        it('should return -1 when the value is not present', function(){
-
-            console.log(React);
-
-            assert.equal(-1, [1,2,3].indexOf(5));
-            assert.equal(-1, [1,2,3].indexOf(0));
-        })
-    })
-})
+  it('render', () => {
+    var html = React.renderToString(App);
+    expect(html).to.contain('hello world')
+  });
+});
